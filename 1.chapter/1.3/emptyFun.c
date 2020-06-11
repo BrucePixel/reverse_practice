9cd2e096c262a9d81ee3573816d0560c77a33134
Bruce <Bruce62020@outlook.com>
2020-06-11 16:13:13 +0800

    Practice for Windows.

--- 1.chapter/1.3/emptyFun.c
+++ 1.chapter/1.3/emptyFun.c
@@ -1,7 +1,12 @@
 /* compile this file using following command
  * 
- * command: gcc -S emptyFun.c
- * output: emptyFun.s
+ * Platform: Linux/Ubuntu
+ * Command: gcc -S emptyFun.c
+ * Output: emptyFun.s
+ *
+ * Platform: Windows 10
+ * Command: cl.exe /Fa emptyFun.c
+ * Output: emptyFun.asm emptyFun.obj
  *
  */
 
