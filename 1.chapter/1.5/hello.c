d5c8fdb1258cfe0d3e5b631fdf2a2ad611678ffe
Bruce <Bruce62020@outlook.com>
2020-06-17 15:14:54 +0800

    Solution for Linux.

--- 1.chapter/1.5/hello.c
+++ 1.chapter/1.5/hello.c
@@ -1,12 +1,12 @@
 /* compile this file using following command
  * 
  * Platform: Linux/Ubuntu
- * Command: gcc -S emptyFun.c
- * Output: emptyFun.s
+ * Command: gcc -S -fno-asynchronous-unwind-tables hello.c -o linux.s
+ * Output: linux.s
  *
  * Platform: Windows 10
- * Command: cl.exe /Fawin.asm emptyFun.c
- * Output: emptyFun.asm emptyFun.obj
+ * Command: cl.exe /Fawin.asm hello.c
+ * Output: win.asm win.obj
  *
  */
 
---
+++ 1.chapter/1.5/linux.s
@@ -0,0 +1,19 @@
+	.file	"hello.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"hello, world"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	popq	%rbp
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
